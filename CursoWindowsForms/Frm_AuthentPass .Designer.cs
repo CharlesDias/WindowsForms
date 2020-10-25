bc799a6a936dee338ffffb9d432bd2da0197eef8
Charles Dias <charles.dias@virtus.ufcg.edu.br>
2020-10-25 15:03:38 -0300

    Course 01 - Class 03.11

---
+++ CursoWindowsForms/Frm_AuthentPass .Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_AuthentPass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Password = new System.Windows.Forms.TextBox();
+            this.Lbl_Result = new System.Windows.Forms.Label();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Btn_ShowPassword = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Txt_Password
+            // 
+            this.Txt_Password.Location = new System.Drawing.Point(12, 45);
+            this.Txt_Password.Name = "Txt_Password";
+            this.Txt_Password.PasswordChar = '*';
+            this.Txt_Password.Size = new System.Drawing.Size(218, 20);
+            this.Txt_Password.TabIndex = 0;
+            this.Txt_Password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_Password_KeyDown);
+            // 
+            // Lbl_Result
+            // 
+            this.Lbl_Result.AutoSize = true;
+            this.Lbl_Result.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Result.Location = new System.Drawing.Point(12, 90);
+            this.Lbl_Result.Name = "Lbl_Result";
+            this.Lbl_Result.Size = new System.Drawing.Size(0, 22);
+            this.Lbl_Result.TabIndex = 1;
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Location = new System.Drawing.Point(245, 42);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(111, 23);
+            this.Btn_Reset.TabIndex = 2;
+            this.Btn_Reset.Text = "Clean";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_Reset_Click);
+            // 
+            // Btn_ShowPassword
+            // 
+            this.Btn_ShowPassword.Location = new System.Drawing.Point(245, 71);
+            this.Btn_ShowPassword.Name = "Btn_ShowPassword";
+            this.Btn_ShowPassword.Size = new System.Drawing.Size(111, 23);
+            this.Btn_ShowPassword.TabIndex = 3;
+            this.Btn_ShowPassword.Text = "Show Password";
+            this.Btn_ShowPassword.UseVisualStyleBackColor = true;
+            this.Btn_ShowPassword.Click += new System.EventHandler(this.Btn_ShowPassword_Click);
+            // 
+            // Frm_AuthentPass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.Btn_ShowPassword);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.Lbl_Result);
+            this.Controls.Add(this.Txt_Password);
+            this.Name = "Frm_AuthentPass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Authenticate Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Password;
+        private System.Windows.Forms.Label Lbl_Result;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Button Btn_ShowPassword;
+    }
+}
---
+++ CursoWindowsForms/Frm_AuthentPass .cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_AuthentPass : Form
+    {
+
+        bool ShowPasswordTxt = false;
+
+        public Frm_AuthentPass()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Password.Text = "";
+            Lbl_Result.Text = "";
+        }
+
+        private void Txt_Password_KeyDown(object sender, KeyEventArgs e)
+        {
+            ChecaForcaSenha checkPass = new ChecaForcaSenha();
+            ChecaForcaSenha.ForcaDaSenha strongPass;
+            strongPass = checkPass.GetForcaDaSenha(Txt_Password.Text);
+            Lbl_Result.Text = strongPass.ToString();
+
+            if(Lbl_Result.Text == "Inaceitavel" || Lbl_Result.Text == "Fraca")
+            {
+                Lbl_Result.ForeColor = Color.Red;
+            }
+            else if (Lbl_Result.Text == "Aceitavel")
+            {
+                Lbl_Result.ForeColor = Color.Yellow;
+            }
+            else if (Lbl_Result.Text == "Forte" || Lbl_Result.Text == "Segura")
+            {
+                Lbl_Result.ForeColor = Color.Green;
+            }
+        }
+
+        private void Btn_ShowPassword_Click(object sender, EventArgs e)
+        {
+            if(ShowPasswordTxt == false)
+            {
+                Btn_ShowPassword.Text = "Hide Password";
+                Txt_Password.PasswordChar = '\0';
+                ShowPasswordTxt = true;
+            }
+            else
+            {
+                Btn_ShowPassword.Text = "Show Password";
+                Txt_Password.PasswordChar = '*';
+                ShowPasswordTxt = false;
+            }
+        }
+    }
+
+
+    public class ChecaForcaSenha
+    {
+
+        public enum ForcaDaSenha
+        {
+            Inaceitavel,
+            Fraca,
+            Aceitavel,
+            Forte,
+            Segura
+        }
+
+        public int geraPontosSenha(string senha)
+        {
+            if (senha == null) return 0;
+            int pontosPorTamanho = GetPontoPorTamanho(senha);
+            int pontosPorMinusculas = GetPontoPorMinusculas(senha);
+            int pontosPorMaiusculas = GetPontoPorMaiusculas(senha);
+            int pontosPorDigitos = GetPontoPorDigitos(senha);
+            int pontosPorSimbolos = GetPontoPorSimbolos(senha);
+            int pontosPorRepeticao = GetPontoPorRepeticao(senha);
+            return pontosPorTamanho + pontosPorMinusculas + pontosPorMaiusculas + pontosPorDigitos + pontosPorSimbolos - pontosPorRepeticao;
+        }
+
+        private int GetPontoPorTamanho(string senha)
+        {
+            return Math.Min(10, senha.Length) * 7;
+        }
+
+        private int GetPontoPorMinusculas(string senha)
+        {
+            int rawplacar = senha.Length - Regex.Replace(senha, "[a-z]", "").Length;
+            return Math.Min(2, rawplacar) * 5;
+        }
+
+        private int GetPontoPorMaiusculas(string senha)
+        {
+            int rawplacar = senha.Length - Regex.Replace(senha, "[A-Z]", "").Length;
+            return Math.Min(2, rawplacar) * 5;
+        }
+
+        private int GetPontoPorDigitos(string senha)
+        {
+            int rawplacar = senha.Length - Regex.Replace(senha, "[0-9]", "").Length;
+            return Math.Min(2, rawplacar) * 6;
+        }
+
+        private int GetPontoPorSimbolos(string senha)
+        {
+            int rawplacar = Regex.Replace(senha, "[a-zA-Z0-9]", "").Length;
+            return Math.Min(2, rawplacar) * 5;
+        }
+
+        private int GetPontoPorRepeticao(string senha)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(\w)*.*\1");
+            bool repete = regex.IsMatch(senha);
+            if (repete)
+            {
+                return 30;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        public ForcaDaSenha GetForcaDaSenha(string senha)
+        {
+            int placar = geraPontosSenha(senha);
+
+            if (placar < 50)
+                return ForcaDaSenha.Inaceitavel;
+            else if (placar < 60)
+                return ForcaDaSenha.Fraca;
+            else if (placar < 80)
+                return ForcaDaSenha.Aceitavel;
+            else if (placar < 100)
+                return ForcaDaSenha.Forte;
+            else
+                return ForcaDaSenha.Segura;
+        }
+    }
+}
