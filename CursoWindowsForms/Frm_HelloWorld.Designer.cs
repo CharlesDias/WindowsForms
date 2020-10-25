0e9ca1916b5e803a1882807f886d33cdac41b1fd
Charles Dias <charles.dias@virtus.ufcg.edu.br>
2020-10-25 10:19:23 -0300

    Course 01 - Class 02

---
+++ CursoWindowsForms/Frm_HelloWorld.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_HelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_ChangeLabel = new System.Windows.Forms.Button();
+            this.txt_LabelContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.Location = new System.Drawing.Point(30, 30);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(213, 19);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "Visual Studio .NET Version";
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(162, 246);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(150, 23);
+            this.btn_Exit.TabIndex = 1;
+            this.btn_Exit.Text = "Exit Application";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_ChangeLabel
+            // 
+            this.btn_ChangeLabel.Location = new System.Drawing.Point(34, 75);
+            this.btn_ChangeLabel.Name = "btn_ChangeLabel";
+            this.btn_ChangeLabel.Size = new System.Drawing.Size(150, 23);
+            this.btn_ChangeLabel.TabIndex = 2;
+            this.btn_ChangeLabel.Text = "Change Text Label";
+            this.btn_ChangeLabel.UseVisualStyleBackColor = true;
+            this.btn_ChangeLabel.Click += new System.EventHandler(this.btn_ChangeLabel_Click);
+            // 
+            // txt_LabelContent
+            // 
+            this.txt_LabelContent.Location = new System.Drawing.Point(34, 125);
+            this.txt_LabelContent.Name = "txt_LabelContent";
+            this.txt_LabelContent.Size = new System.Drawing.Size(314, 20);
+            this.txt_LabelContent.TabIndex = 3;
+            // 
+            // Frm_HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(464, 281);
+            this.Controls.Add(this.txt_LabelContent);
+            this.Controls.Add(this.btn_ChangeLabel);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.lbl_Title);
+            this.Name = "Frm_HelloWorld";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_ChangeLabel;
+        private System.Windows.Forms.TextBox txt_LabelContent;
+    }
+}
+
---
+++ CursoWindowsForms/Frm_HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_HelloWorld : Form
+    {
+        public Frm_HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_ChangeLabel_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_LabelContent.Text) && !string.IsNullOrWhiteSpace(txt_LabelContent.Text))
+            {
+                lbl_Title.Text = txt_LabelContent.Text;
+            }
+        }
+    }
+}
