a32482ae179a81b73e46a0b83d2fce0a0a7c7efd
Charles Dias <charles.dias@virtus.ufcg.edu.br>
2020-10-26 20:18:18 -0300

    Course 01 - Class 04.06

---
+++ CursoWindowsForms/Frm_CheckCPF.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_CheckCPF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Msk_CPF = new System.Windows.Forms.MaskedTextBox();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Btn_Check = new System.Windows.Forms.Button();
+            this.Lbl_Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Msk_CPF
+            // 
+            this.Msk_CPF.Location = new System.Drawing.Point(12, 12);
+            this.Msk_CPF.Mask = "000,000,000-00";
+            this.Msk_CPF.Name = "Msk_CPF";
+            this.Msk_CPF.Size = new System.Drawing.Size(218, 20);
+            this.Msk_CPF.TabIndex = 0;
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Location = new System.Drawing.Point(245, 42);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Reset.TabIndex = 1;
+            this.Btn_Reset.Text = "Reset";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_reset_Click);
+            // 
+            // Btn_Check
+            // 
+            this.Btn_Check.Location = new System.Drawing.Point(245, 71);
+            this.Btn_Check.Name = "Btn_Check";
+            this.Btn_Check.Size = new System.Drawing.Size(111, 23);
+            this.Btn_Check.TabIndex = 2;
+            this.Btn_Check.Text = "Check";
+            this.Btn_Check.UseVisualStyleBackColor = true;
+            this.Btn_Check.Click += new System.EventHandler(this.Btn_Check_Click);
+            // 
+            // Lbl_Result
+            // 
+            this.Lbl_Result.AutoSize = true;
+            this.Lbl_Result.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Result.Location = new System.Drawing.Point(12, 90);
+            this.Lbl_Result.Name = "Lbl_Result";
+            this.Lbl_Result.Size = new System.Drawing.Size(0, 22);
+            this.Lbl_Result.TabIndex = 3;
+            // 
+            // Frm_CheckCPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.Lbl_Result);
+            this.Controls.Add(this.Btn_Check);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.Msk_CPF);
+            this.Name = "Frm_CheckCPF";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox Msk_CPF;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Button Btn_Check;
+        private System.Windows.Forms.Label Lbl_Result;
+    }
+}
